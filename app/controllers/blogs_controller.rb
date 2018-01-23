04e19d27f91a26177926e9f33a570c9ea9565d9b
Yuki Miyahara <y_miya@nttpc.co.jp>
2018-01-23 08:15:41 +0000

    get_user_id_from_blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,6 +20,7 @@ class BlogsController < ApplicationController
   
   def create
     @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
     if @blog.save
       redirect_to blogs_path, notice: "ブログを作成しました！"
     else
